e037c456418e2c3cd06c72d88b4f3dedbdbb9b85
Oleg <entegrato@gmail.com>
2019-01-09 02:23:50 +0300

    Переподключены модули чтобы и www и pda использовали одну верстку

--- routes/pda.js
+++ routes/pda.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var data = require('../views/partials/data-object.json');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('pages/pda-index', {
     title: "PDA" + ((req.query.rmd) ? ' | RMD' : '') + '' + ((req.query.rcol) ? ' | RCol' : ''),
     rmd: ((req.query.rmd) ? true : false),
-    rcol: ((req.query.rcol) ? true : false)
+    rcol: ((req.query.rcol) ? true : false),
+    data: data
   });
 });
 
